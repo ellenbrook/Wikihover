d32bd9e3df75e47e2876b5e0fcdbcfc36f35bb76
Eric <ericellenbrook@gmail.com>
2014-11-14 14:22:01 -0500

    Check for image link

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,36 +4,12 @@ chrome.extension.sendMessage({}, function(response) {
 		clearInterval(readyStateCheckInterval);
 
 		$('#bodyContent a').on('mouseenter', function(event){
-			//Clear the data
-			clearData();
-			//Remove the title attribute    
-			$(this).removeAttr('title');
 
-			var baseURL = window.location.origin, //Get the URL
-			    url     = $(this).attr('href'), //Get the href of the link
-			    fullURL = baseURL+url, //Combine those two together to get the link
-			    x = event.pageX, // Get x position of mouse
-			    y = event.pageY; // get right position of mouse
-			    positionLeft = 10+"px", //set the position
-			    positionRight = 10+"px", //Set the right position 
-			    positionTop = 10+"px"; //set the position
+			if($(this).children('img').length === 0)
+			{
+				performWork($(this));
+			}
 
-			$.get(fullURL, function(data) {
-			    var content = $(data).find('#bodyContent').find('p').html(),
-			        cssRules = {
-			        	position: 'fixed', 
-			        	top: positionTop, 
-			        	left: positionLeft, 
-			        	right: positionRight,
-			        	maxHeight: 500+"px",
-			        	padding: 1.4+"em",
-			        	background: "#fff",
-			        	boxShadow: "2px 2px 15px rgba(50,50,50,.75)",
-			        	zIndex: 100
-			        };
-
-			    $('<div id="popup-notification">'+content+'</div>').css(cssRules).appendTo('body').fadeIn();
-			});
 		});
 
 
@@ -49,6 +25,37 @@ chrome.extension.sendMessage({}, function(response) {
 		{
 			$('#popup-notification').remove();
 			$(this).attr('title', $(this).attr('data-title'));
+		}
+
+		function performWork(element)
+		{
+			//Clear the data
+			clearData();
+			//Remove the title attribute    
+			element.removeAttr('title');
+
+			var baseURL = window.location.origin, //Get the URL
+			    url     = element.attr('href'), //Get the href of the link
+			    fullURL = baseURL+url, //Combine those two together to get the link
+			    x = event.pageX, // Get x position of mouse
+			    y = event.pageY; // get right position of mouse
+
+			$.get(fullURL, function(data) {
+			    var content = $(data).find('#bodyContent').find('p').html(),
+			        cssRules = {
+			        	position: 'fixed', 
+			        	top: 10+"px", 
+			        	left: 10+"px", 
+			        	right: 10+"px",
+			        	maxHeight: 500+"px",
+			        	padding: 1.4+"em",
+			        	background: "#fff",
+			        	boxShadow: "2px 2px 15px rgba(50,50,50,.75)",
+			        	zIndex: 100
+			        };
+
+			    $('<div id="popup-notification">'+content+'</div>').css(cssRules).appendTo('body').fadeIn();
+			});
 		}
 	}
 	}, 10);
